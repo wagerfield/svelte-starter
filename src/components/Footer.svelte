9df072e879ffc5ec0647761762d08dd1df570478
Matthew Wagerfield <matthew@wagerfield.com>
2023-09-20 23:16:19 +0100

    style: update footer text color

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,7 +2,7 @@
   import { timeStore } from "$stores/time.store"
 </script>
 
-<footer class="flex px-4 py-3 bg-slate-900 text-slate-500">
+<footer class="flex px-4 py-3 bg-slate-900 text-slate-200">
   {$timeStore.toLocaleString("en", {
     hour12: false,
     hour: "numeric",
