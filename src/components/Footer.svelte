4723374fe2c344604128199268335a5c20ac9806
Matthew Wagerfield <matthew@wagerfield.com>
2023-09-20 23:21:53 +0100

    style: update footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
   import { timeStore } from "$stores/time.store"
-</script>
 
-<footer class="flex px-4 py-3 bg-slate-900 text-slate-200">
-  {$timeStore.toLocaleString("en", {
+  $: time = $timeStore.toLocaleString("en", {
     hour12: false,
     hour: "numeric",
     minute: "numeric",
     second: "numeric",
-  })}
+  })
+</script>
+
+<footer class="flex px-4 py-3 bg-black text-slate-300">
+  <span class="ml-auto">
+    {time}
+  </span>
 </footer>
