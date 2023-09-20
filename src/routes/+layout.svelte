a502a8fce5f6c2e88758dbe5ad0f7eec4932c474
Matthew Wagerfield <matthew@wagerfield.com>
2023-09-20 22:41:01 +0100

    feat: add index layout and page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import "../app.css"
+
+  import "@fontsource-variable/inter"
+  import "@fontsource-variable/jetbrains-mono"
+
+  import Head from "$components/Head.svelte"
+  import Header from "$components/Header.svelte"
+  import Footer from "$components/Footer.svelte"
+</script>
+
+<Head />
+
+<Header />
+<main>
+  <slot />
+</main>
+<Footer />
