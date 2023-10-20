a43d850fe83f9d4823c7128e1bfe739d3ebf6237
Matthew Wagerfield <matthew@wagerfield.com>
2023-10-20 22:22:58 +0100

    feat: update root layout and page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,4 @@
-<script>
-  import { Auth } from "@supabase/auth-ui-svelte"
-
-  import Heading from "$components/Heading.svelte"
-
-  export let data
-</script>
-
-<Heading>Make something wonderful.</Heading>
-
-<a href="/jedi" class="btn btn-primary">Take me somewhere</a>
-
-<Auth
-  view="magic_link"
-  showLinks={false}
-  supabaseClient={data.supabase}
-  redirectTo={`${data.url}/auth/callback`}
-/>
+<ul>
+  <li><a href="/signup">Sign Up</a></li>
+  <li><a href="/signin">Sign In</a></li>
+</ul>
