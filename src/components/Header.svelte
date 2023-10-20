53adeb36e736ac37fa7d4f6a9cd56abbad3562c1
Matthew Wagerfield <matthew@wagerfield.com>
2023-10-20 22:25:43 +0100

    feat: add auth links to header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
+  import { page } from "$app/stores"
   import Clock from "./Clock.svelte"
 </script>
 
 <header class="shrink-0 flex justify-between px-4 py-3">
   <nav class="flex space-x-2">
     <a href="/">Home</a>
-    <span class="text-gray-300">/</span>
-    <a href="/jedi">Jedi</a>
   </nav>
   <span><Clock /></span>
+  <nav class="flex space-x-2">
+    {#if $page.data.session}
+      <a href="/signout">Sign out</a>
+    {:else}
+      <a href="/signup">Sign up</a>
+      <span class="text-gray-300">/</span>
+      <a href="/signin">Sign in</a>
+    {/if}
+  </nav>
 </header>
