3f95342ade59c8fb378a24ea063916efed02619d
Matthew Wagerfield <matthew@wagerfield.com>
2023-10-20 22:23:17 +0100

    chore: remove jedi page

--- src/routes/jedi/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-  import Heading from "$components/Heading.svelte"
-</script>
-
-<Heading>This is the page you are looking.</Heading>
-<a href="/" class="btn btn-primary">Take me back again</a>
