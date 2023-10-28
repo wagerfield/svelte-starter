75459bc1bb85e1603ac73b53a59485c1afe60e9c
Matthew Wagerfield <matthew@wagerfield.com>
2023-10-29 00:15:59 +0100

    feat: create form components

---
+++ src/components/SubmitButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { getForm } from "formsnap"
+
+  const { submitting, allErrors } = getForm()
+</script>
+
+<button
+  type="submit"
+  class="btn btn-primary w-full disabled:btn-disabled"
+  disabled={Boolean($submitting || $allErrors.length)}
+>
+  <slot />
+</button>
