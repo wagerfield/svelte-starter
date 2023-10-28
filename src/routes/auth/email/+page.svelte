e4485df44cf7f11ca76b8b58af8fd91f1601b1ad
Matthew Wagerfield <matthew@wagerfield.com>
2023-10-29 00:20:44 +0100

    feat: add auth email page

---
+++ src/routes/auth/email/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { onMount } from "svelte"
+  import { goto } from "$app/navigation"
+
+  export let data
+
+  onMount(() => {
+    const result = data.supabase.auth.onAuthStateChange((event, session) => {
+      if (session) goto("/dashboard")
+    })
+
+    return () => result.data.subscription.unsubscribe()
+  })
+</script>
+
+<p>Check your inbox</p>
