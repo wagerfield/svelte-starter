44d2b9c1bc41e86c6402fe1b7dbd70ccd2479c98
Matthew Wagerfield <matthew@wagerfield.com>
2023-10-29 00:15:59 +0100

    feat: create form components

---
+++ src/components/FormMessage.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { getForm } from "formsnap"
+
+  let tag: string | undefined = "p"
+  let className: string | undefined = undefined
+
+  export { className as class }
+
+  const { message } = getForm()
+</script>
+
+{#if $message}
+  <svelte:element this={tag} class={className}>{$message}</svelte:element>
+{/if}
---
+++ src/components/SignOutButton.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { enhance } from "$app/forms"
+
+  let className: string | undefined = undefined
+
+  export { className as class }
+</script>
+
+<form method="POST" action="/?/signOut" use:enhance>
+  <button class={className}>Sign Out</button>
+</form>
