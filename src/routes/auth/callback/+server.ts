b533d12cbc4df413121e577c6565e874c9f30d7e
Matthew Wagerfield <matthew@wagerfield.com>
2023-10-29 00:19:30 +0100

    feat: refine auth callback logic

--- src/routes/auth/callback/+server.ts
+++ src/routes/auth/callback/+server.ts
@@ -1,11 +1,9 @@
 import { redirect } from "@sveltejs/kit"
 
-export const GET = async ({ url, locals }) => {
+export const GET = async ({ url, locals: { supabase } }) => {
   const code = url.searchParams.get("code")
 
-  if (code) {
-    await locals.supabase.auth.exchangeCodeForSession(code)
-  }
+  if (code) await supabase.auth.exchangeCodeForSession(code)
 
-  throw redirect(303, "/account")
+  throw redirect(303, "/dashboard")
 }
