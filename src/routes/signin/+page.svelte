8446d5fdb38221064796b29fe2c89cf7ce9f56a5
Matthew Wagerfield <matthew@wagerfield.com>
2023-10-30 11:40:04 +0000

    feat: integrate supabase binding component

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
   import { Form } from "formsnap"
   import { signInSchema } from "./schemas"
-
-  import FormMessage from "$components/FormMessage.svelte"
-  import SubmitButton from "$components/SubmitButton.svelte"
+  import { FormMessage, SubmitButton } from "$components/form"
 
   export let data
 </script>
