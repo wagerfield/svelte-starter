5d6d8b099f48eb02fd81de25520ffe041ff0562e
Matthew Wagerfield <matthew@wagerfield.com>
2023-10-30 11:39:38 +0000

    feat: create supabase binding component

---
+++ src/components/form/index.ts
@@ -0,0 +1,5 @@
+import FormMessage from "./FormMessage.svelte"
+import SubmitButton from "./SubmitButton.svelte"
+import SupabaseBinding from "./SupabaseBinding.svelte"
+
+export { FormMessage, SubmitButton, SupabaseBinding }
