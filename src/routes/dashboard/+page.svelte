92a79622a9520b9605ca0f86e4285f7843719f4c
Matthew Wagerfield <matthew@wagerfield.com>
2023-10-30 21:51:48 +0000

    feat: add table and column props

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -4,6 +4,9 @@
   import { FormMessage, SubmitButton, SupabaseBinding } from "$components/form"
 
   export let data
+
+  $: userId = data.session.user.id
+  $: filter = `id=eq.${userId}`
 </script>
 
 <div class="space-y-8 w-80">
@@ -14,13 +17,13 @@
         <Form.Label {...attrs.label}>Name</Form.Label>
         <Form.Input {...attrs.input} placeholder="First Last" />
         <Form.Validation {...attrs.validation} />
-        <SupabaseBinding />
+        <SupabaseBinding table="user_profiles" column="name" {filter} />
       </div>
     </Form.Field>
     <Form.Field {config} name="email" let:attrs>
       <div>
         <Form.Label {...attrs.label}>Email</Form.Label>
-        <Form.Input {...attrs.input} placeholder="name@example.com" readonly />
+        <Form.Input {...attrs.input} readonly />
       </div>
     </Form.Field>
     <FormMessage />
